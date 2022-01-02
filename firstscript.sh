eef024cf251b7323b189c8fcc517530f4dfa6fa1
Queen Nnakwue <qnnakwue@gmail.com>
2022-01-02 20:09:44 +0100

    This script prints information about a system

---
+++ firstscript.sh
@@ -0,0 +1,22 @@
+!#/bin/bash
+
+### This script prints system info
+
+echo "Welcome to bash script"
+echo
+echo "########################################"
+echo "The uptime of the system is: "
+uptime
+echo
+
+
+echo "#########################################"
+echo "Memory Utlilization"
+free -m
+echo
+
+
+echo "#########################################"
+echo "Disk Utilization"
+df -h
+~
