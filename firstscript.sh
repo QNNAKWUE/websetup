8b575803ea7461e033f02f8673f80e8e8f0084c8
Queen Nnakwue <qnnakwue@gmail.com>
2022-01-02 20:17:14 +0100

    made updates

--- firstscript.sh
+++ firstscript.sh
@@ -2,20 +2,20 @@
 
 ### This script prints system info
 
-echo "Welcome to bash script"
-echo
+
+#checking system uptime
 echo "########################################"
 echo "The uptime of the system is: "
 uptime
 echo
 
-
-echo "#########################################"
+#Memory Utilization
+echo "#############################################"
 echo "Memory Utlilization"
 free -m
 echo
 
-
+#Disk Utilization
 echo "#########################################"
 echo "Disk Utilization"
 df -h
